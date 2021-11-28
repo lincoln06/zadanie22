e5ae78bd6a654ee880262ee2797ccf7c2f8210ca
MJL <maciekjl1993@gmail.com>
2021-11-28 14:43:04 +0100

    1

---
+++ zadanie22/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace zadanie22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj pierwszy produkt");
+            List<string> lista = new List<string>();
+            string produkt = Console.ReadLine();
+            lista.Add(produkt);
+            char wybor = 't';
+            int lp = 1;
+            while(wybor!='n'&&wybor!='N')
+            {
+                Console.Write("Czy chcesz dodać kolejny produkt? (T/N) ");
+                wybor = char.Parse(Console.ReadLine());
+                while (wybor == 't' || wybor == 'T')
+                {
+                    Console.Write("Nazwa:\t");
+                    produkt = Console.ReadLine();
+                    lista.Add(produkt);
+                    Console.Write("Czy chcesz dodać kolejny produkt? (T/N) ");
+                    wybor = char.Parse(Console.ReadLine());
+                }
+            }
+            lista.Sort();
+            Console.WriteLine("\nTwoja lista zakupów:\n");
+            foreach(var e in lista)
+            {
+                Console.WriteLine($"{lp}\t{e}");
+                lp++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
